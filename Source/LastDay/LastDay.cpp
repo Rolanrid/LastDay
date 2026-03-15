fab12dbf37410fc420b0a6003637653ed6261eeb
Rolanrid <2539706678@qq.com>
2026-03-16 03:24:56 +0800

    [Desc] init

---
+++ Source/LastDay/LastDay.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LastDay.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LastDay, "LastDay" );
+
+DEFINE_LOG_CATEGORY(LogLastDay)
